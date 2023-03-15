63350729786e8036d0b2ae058e1f9bd5b1a882c0
thekhai <thekhai1k9@gmail.com>
2023-03-15 15:30:29 +0700

    [setup enviroment

---
+++ src/server.js
@@ -0,0 +1,20 @@
+//const express = require("express")
+import express from "express"
+import configViewEngine from "./configs/viewEngine"
+require("dotenv").config()
+
+
+const app = express()
+const port = process.env.PORT || 8080
+
+
+configViewEngine(app)
+
+app.get('/', (req, res) => {
+    res.render("index.ejs")
+})
+
+
+app.listen(port, () => {
+    console.log(`ok on port ${port}`)
+})
